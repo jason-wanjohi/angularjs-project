52fbc52b368a79412e4c3bc40effee28a143a4fb
Jason Wanjohi <wanjohijason@gmail.com>
2022-08-17 01:05:35 +0300

    Initial commit

---
+++ app/view1/view1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('myApp.view1', ['ui.router'])
+
+.config(['$stateProvider', function($stateProvider) {
+  $stateProvider.state({
+    name: 'view1',
+    url: '/view1',
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', [function() {
+
+}]);
